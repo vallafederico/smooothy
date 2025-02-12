7ca1a3922c4399639b02d0117d474a8710c7fe63
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-02-12 15:27:22 +0100

    restructure the shit out of this

--- web/src/js/app.js
+++ web/src/js/app.js
@@ -2,11 +2,11 @@ import { Slider } from "./slider";
 import Scroll from "./scroll";
 import gsap from "./gsap";
 
-const s1 = document.querySelector("[data-slider='1']");
-let slider1;
-if (s1) {
-  slider1 = new Slider(s1);
-}
+// const s1 = document.querySelector("[data-slider='1']");
+// let slider1;
+// if (s1) {
+//   slider1 = new Slider(s1);
+// }
 
 // setTimeout(() => {
 //   console.log("set snap");
@@ -18,9 +18,9 @@ if (s1) {
 //   }, 5000);
 // }, 5000);
 
-const s2 = document.querySelector("[data-slider='2']");
-if (s2) {
-  const slider2 = new Slider(s2, {
-    infinite: false,
-  });
-}
+// const s2 = document.querySelector("[data-slider='2']");
+// if (s2) {
+//   const slider2 = new Slider(s2, {
+//     infinite: false,
+//   });
+// }
