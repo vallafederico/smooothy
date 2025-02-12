d994935e1fd8e28d9c803d057b4149705c7e2468
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-02-12 15:42:27 +0100

    wild html and css here

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,19 +1,22 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import { qrcode } from "vite-plugin-qrcode";
+import { defineConfig } from "astro/config"
+import { qrcode } from "vite-plugin-qrcode"
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
-import solidJs from "@astrojs/solid-js";
+import solidJs from "@astrojs/solid-js"
 
-import vue from "@astrojs/vue";
-import preact from "@astrojs/preact";
+import vue from "@astrojs/vue"
+import preact from "@astrojs/preact"
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss(), qrcode()],
   },
+  experimental: {
+    svg: true,
+  },
 
   integrations: [solidJs(), vue(), preact()],
-});
+})
