60e885b32f56ad8667bfb09200513185cd455aab
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-02-12 12:52:43 +0100

    hih hoho

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { qrcode } from "vite-plugin-qrcode";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss(), qrcode()],
+  },
 });
