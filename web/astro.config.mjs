7bb8c22fbfb617c71e3e09f23f0ef1ca154a8515
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-03-04 16:40:02 +0100

    remove weird log

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,8 +4,6 @@ import { qrcode } from "vite-plugin-qrcode"
 
 import tailwindcss from "@tailwindcss/vite"
 
-// log the current path
-console.log(import.meta.dirname)
 // https://astro.build/config
 export default defineConfig({
   vite: {
