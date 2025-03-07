cf92d1990a15576678f16851b53dce7dec3004e0
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-03-08 00:45:33 +0100

    animaten logo

--- web/src/js/modules/logo.ts
+++ web/src/js/modules/logo.ts
@@ -1,5 +1,36 @@
+import gsap from "../gsap"
+
 export class Logo {
+  element: HTMLElement
+  svg: SVGElement[]
+
   constructor(element: HTMLElement) {
-    // console.log(element)
+    this.element = element
+    this.svg = [...element.querySelector("svg").children] as SVGElement[]
+    this.init()
+  }
+
+  init() {
+    this.element.onmouseenter = () => {
+      gsap.to(this.svg, {
+        duration: 0.6,
+        yPercent: (i: number) =>
+          i % 2 === 0 ? Math.random() * 25 : -Math.random() * 20,
+        stagger: 0.02,
+      })
+    }
+
+    this.element.onmouseleave = () => {
+      gsap.to(this.svg, {
+        duration: 0.6,
+        yPercent: 0,
+        stagger: 0.02,
+      })
+    }
+  }
+
+  destroy() {
+    this.element.onmouseenter = null
+    this.element.onmouseleave = null
   }
 }
