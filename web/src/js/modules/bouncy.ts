66b99bad6f74027933aaaf94894e31ae982fcda4
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-03-08 12:57:30 +0100

    bumpy

---
+++ web/src/js/modules/bouncy.ts
@@ -0,0 +1,10 @@
+export class Bouncy {
+  svg: SVGGElement[]
+
+  constructor(private element: HTMLElement) {
+    this.svg = [...this.element.querySelector("svg").children].filter(
+      (el): el is SVGGElement => el instanceof SVGGElement
+    )
+    console.log(this.svg)
+  }
+}
