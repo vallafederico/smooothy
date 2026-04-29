e16163979d5825a2d1452cf309bc33eb77017c7d
Federico Valla <valla.federico@gmail.com>
2026-04-29 22:29:15 +0200

    working omnidirectional

--- web/src/wip/slider/omnidirectional.js
+++ web/src/wip/slider/omnidirectional.js
@@ -5,9 +5,43 @@ export class Slider extends Core {
   constructor(wrapper, config = {}) {
     super(wrapper, {
       ...config,
+      vertical: true,
     })
 
+    this.rows = this.items
+
+    this.hCore = new Core(wrapper, {
+      ...config,
+      vertical: false,
+    })
+
+    const firstRow = this.rows[0]
+    if (firstRow) {
+      this.hCore.items = [...firstRow.children]
+      this.hCore.resize()
+    }
+
     gsap.ticker.add(this.update.bind(this))
+    gsap.ticker.add(this.updateHorizontal.bind(this))
+  }
+
+  updateHorizontal() {
+    if (!this.hCore) return
+    this.hCore.update()
+
+    const masterItems = this.hCore.items
+    for (let r = 1; r < this.rows.length; r++) {
+      const rowSlides = this.rows[r].children
+      for (let c = 0; c < masterItems.length; c++) {
+        const slide = rowSlides[c]
+        if (slide) slide.style.transform = masterItems[c].style.transform
+      }
+    }
+  }
+
+  destroy() {
+    super.destroy()
+    if (this.hCore) this.hCore.destroy()
   }
 }
 
